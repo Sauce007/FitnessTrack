f4c01d1933a495c12f17d7e048d88e589c6ca2aa
Tyler Harper <60906140+Sauce007@users.noreply.github.com>
2020-12-02 20:32:26 -0600

    Add files via upload

---
+++ components/Routines.js
@@ -0,0 +1,52 @@
+import { Card } from "@material-ui/core";
+import React, {useState, useEffect} from "react";
+import {hitAPI} from "../api/index";
+import "./routines.css";
+
+const Routines = () => {
+    const [routinesList, setRoutinesList] = useState([]);
+
+    useEffect(() => {
+        hitAPI("GET", "/routines")
+            .then((data) => {
+                console.log(data)
+                setRoutinesList(data);
+            })
+            .catch((error) => {
+                console.error("There was a problem getting your routines", error);
+            })
+    }, []);
+
+    return (
+        <>
+            <div className="routines-page">
+            <h1>View Public Routines...</h1>
+            <br></br>
+            {routinesList.map((routine) => {
+                return (
+                    <Card className="routine-card" key={routine.id, routine.creatorID, routine.isPublic}>
+                        <h2 className="routine-header">{routine.creatorName}'s {routine.name}</h2>
+                        <p className="routine-goal">Routine Goal: {routine.goal}</p>
+                        {routine.activities.map((activity) => {
+                            return <div key={activity.id}>
+                            <p className="activity-name">Activity: {activity.name}</p>
+                            <p className="activity-description">Description: {activity.description}</p>
+                            <p className="activity-duration">Duration: {activity.duration}</p>
+                            <p className="activity-count">Count: {activity.count}</p>
+                            </div>
+                        })}
+                        
+                        
+                    </Card>
+                );
+            })}
+            </div>
+            
+            
+        </>
+    )
+
+
+}
+
+export default Routines;
